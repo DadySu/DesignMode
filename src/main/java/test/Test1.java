053cfc3274d29d06071ded9d9f8b31ef19cc0db5
liubo <672304928@qq.com>
2019-04-17 19:32:36 +0800

    test2

--- src/main/java/test/Test1.java
+++ src/main/java/test/Test1.java
@@ -10,5 +10,10 @@ public class Test1 {
 
     public static void main(String[] args) {
         System.out.println("123");
+        test();
+    }
+
+    public static void test(){
+        System.out.println("456");
     }
 }
