9eaa66f306fbfcc7ab8f1a9fce824cfd36e97ad8
DadySu <liubohumor@sina.com>
2019-04-17 22:47:26 +0800

    Test1提交

---
+++ src/main/java/Test1.java
@@ -0,0 +1,12 @@
+/**
+ * Description
+ *
+ * @author liub
+ * @date 2019-04-17 22:39
+ */
+public class Test1 {
+
+    public static void main(String[] args) {
+        System.out.println("test1");
+    }
+}
