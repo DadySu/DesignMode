9cde4e5208ba06aae170912c46d3ddcdfd87c1e7
liubo <672304928@qq.com>
2019-04-17 19:26:33 +0800

    提交1

---
+++ src/main/java/test/Test1.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * Description
+ *
+ * @author liub
+ * @date 2019-04-17 19:25
+ */
+public class Test1 {
+
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
